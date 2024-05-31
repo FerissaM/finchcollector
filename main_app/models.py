ed1c8be22e88771a616abf4a6daa08859b9179ca
FerissaM <mabalotferissa@gmail.com>
2024-05-31 14:54:22 -0700

    part 3 edited and added part 4

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 class Finch(models.Model):
@@ -9,3 +11,14 @@ class Finch(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('finches_detail', kwargs={'finch_id': self.id})
+    
+class Feeding(models.Model):
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    date = models.DateField(default=date.today)
+    meal = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.date} - {self.finch.name} fed {self.meal}"
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
+from .forms import FeedingForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # Create your views here.
@@ -20,19 +21,35 @@ def finches_index(request):
     return render(request, 'finches/index.html', {'finches': finches})
 
 def finches_detail(request, finch_id):
-    finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    finch = get_object_or_404(Finch, id=finch_id)
+    feedings = finch.feeding_set.all()
+    form = FeedingForm()
+
+    if request.method == 'POST':
+        form = FeedingForm(request.POST)
+        if form.is_valid():
+            new_feeding = form.save(commit=False)
+            new_feeding.finch = finch
+            new_feeding.save()
+            return redirect('finches_detail', finch_id=finch_id)
+
+    return render(request, 'finches/detail.html', {'finch': finch, 'feedings': feedings, 'form': form})
 
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
-    success_url = reverse_lazy('finches_index')
+
+    def get_success_url(self):
+        # Redirect to the detail view of the newly created Finch object
+        return reverse_lazy('finches_detail', kwargs={'finch_id': self.object.pk})
 
 class FinchUpdate(UpdateView):
     model = Finch
     fields = ['species', 'description', 'age']
-    success_url = reverse_lazy('finches_index')
+    template_name = 'main_app/finch_form.html'
 
 class FinchDelete(DeleteView):
     model = Finch
-    success_url = reverse_lazy('finches_index')
+
+    def get_success_url(self):
+        return reverse_lazy('finches_index')
